8fe9e934e5aefc51d98d29d0316daa6ddab07e87
vivianiHarima <viviharima@hotmail.com>
2024-05-31 22:12:58 -0300

    primeiro commit

--- src/classes/Leitor.js
+++ src/classes/Leitor.js
@@ -1,7 +1,51 @@
-class Leitor {
+const crypto = require('crypto');
 
-    constructor() {
-       
+class Leitor {
+    #codigo = 0;
+    #nome = ''
+    #cpf = 0;
+    #rg = 0;
+    #dataNascimento = '';
+
+
+    constructor(codigo, nome, cpf, rg, dataNascimento) {
+       this.#codigo = crypto.randomUUID(),
+       this.#nome = nome,
+       this.#cpf = cpf,
+       this.#rg = rg
+       this.#dataNascimento = dataNascimento
+      
+    }
+
+    get getCodigo(){
+        return this.#codigo
+    }
+    set setCodigo(codigo){
+        this.#codigo = codigo
+    }
+    get getNome(){
+        return this.#nome
+    }
+    set setNome(nome){
+        this.#nome = nome
+    }
+    get getCpf(){
+        return this.#cpf
+    }
+    set setCpf(cpf){
+        this.#cpf = cpf
+    }
+    get getDataNascimento(){
+        return this.#dataNascimento
+    }
+    set setDataNascimento(dataNascimento){
+        this.#dataNascimento = dataNascimento
+    }
+    get getRg(){
+        return this.#rg
+    }
+    set setRg(rg){
+        this.#rg = rg
     }
 
 }
--- src/classes/LeitorCrud.js
+++ src/classes/LeitorCrud.js
@@ -1,9 +1,45 @@
+const fs = require('fs');
 class LeitorCrud {
 
     constructor() {
-       
+       this.filePath = './src/files/leitores.json';
     }
 
+
+    criar(leitor){
+        
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+
+        conteudoAtual.push({
+            codigo: leitor.getCodigo,
+            nome: leitor.getNome,
+            cpf: leitor.getCpf,
+            rg: leitor.getRg,
+            dataNascimento: leitor.getDataNasimento
+        })
+
+        fs.writeFileSync(
+            this.filePath,
+            JSON.stringify(conteudoAtual, null, 2), //null e 2 para salvar organizado
+            'utf-8'
+        )
+    }
+    consultar(consultaNome){
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+        const leitorEncontrado = conteudoAtual.find(leitor => leitor.nome === consultaNome)
+        if(leitorEncontrado){
+            console.log(leitorEncontrado)
+        } else{
+            console.log("Leitor não encontrado")
+        }
+    }
+
+    deletar(deletarNome){
+              // ler o conteudo 
+      //  usar filter para remover o elemento
+      // salva de novo no arquivo o array filtrado
+    }
 }
+
 
 module.exports = LeitorCrud;
